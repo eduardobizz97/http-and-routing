1d008bdb96b7d83fd3689bbc9f015d2434c45456
Eduardo <ebmcangrejo97@gmail.com>
2021-03-11 16:17:00 -0300

    Setting up the project for routing

---
+++ src/containers/Blog/Blog/Blog.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import axios from "../../../axios";
+
+import "./Blog.css";
+import Posts from "./Posts/Posts";
+
+class Blog extends Component {
+
+  render() {
+    return (
+      <div className='Blog'>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/new-post">New Post</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Posts/>
+        {/* <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section> */}
+      </div>
+    );
+  }
+}
+
+export default Blog;
--- src/containers/Blog/Blog/Posts/Posts.js
+++ src/containers/Blog/Blog/Posts/Posts.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
-import axios from "../../axios";
+import axios from '../../../../axios';
 
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
-import "./Blog.css";
+import Post from '../../../../components/Post/Post';
 
-class Blog extends Component {
+class Posts extends Component {
   state = {
     posts: [],
-    selectedPostId: null,
-    error: false,
+    error: false
   };
 
   componentDidMount() {
@@ -27,7 +23,7 @@ class Blog extends Component {
         this.setState({ posts: updatedPosts });
         // console.log(response);
       })
-      .catch((error) => this.setState({ error: true }));
+      .catch((error) =>this.setState({error:true}));
   }
 
   postSelectedHandler = (id) => {
@@ -48,31 +44,8 @@ class Blog extends Component {
         );
       });
     }
-
-    return (
-      <div className='Blog'>
-        <header>
-          <nav>
-            <ul>
-              <li>
-                <a href="/">Home</a>
-              </li>
-              <li>
-                <a href="/new-post">New Post</a>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
-      </div>
-    );
+    return <section className="Posts">{posts}</section>;
   }
 }
 
-export default Blog;
+export default Posts;
